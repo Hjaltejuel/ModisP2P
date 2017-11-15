1e7491d9f416b9fc93d6d20f0f564b2dc34e11b7
Hjaltejuel <hjsk@itu.dk>
2017-11-15 11:41:59 +0100

    We will overcum

---
+++ source/ClientGet.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Michelle on 11/15/2017.
+ */
+public class ClientGet {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
---
+++ source/Node.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Michelle on 11/15/2017.
+ */
+public class Node {
+}
