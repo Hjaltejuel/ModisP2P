10f5c9d661b46711aa3ce3c7d1f746290d3e1f92
Thomas Andersen <thta@itu.dk>
2017-11-15 12:06:12 +0100

    commit

--- source/ClientGet.java
+++ source/ClientGet.java
@@ -1,7 +1,28 @@
+import java.net.Socket;
+
 /**
  * Created by Michelle on 11/15/2017.
  */
 public class ClientGet {
+    String IP;
+    int port;
+    int key;
+    Socket connectSocket;
+    public ClientGet(String IP, int port, int key){
+        connectSocket = null;
+        this.IP = IP;
+        this.port = port;
+        this.key = key;
+        Get();
+    }
+    private void Get(){
+        try{
+            connectSocket = new Socket("IP", port);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args){
         System.out.println("Hello");
     }
